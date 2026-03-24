d122ee109059f0c969e26317f93b33658a47926a
isinha🫧 <isabella.r.rosa@aluno.senai.br>
2026-03-24 13:52:37 -0300

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,6 @@
+let armadura = false;
+let cosmoShiryu = 8000;
+let cosmoInimigo = 7500;
+
+let vitoria = cosmoShiryu >= cosmoInimigo && armadura === false;
+console.log(vitoria);
---
+++ 03.js
@@ -0,0 +1,3 @@
+let nome = prompt('Qual o seu nome de Cavaleiro?');
+let acesso = nome ? 'Permitido' : 'Negado';
+alert('Acesso:'+ acesso);
